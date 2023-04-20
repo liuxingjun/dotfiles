6415824a7b72bbffcaf36f8c04abdfb19f7a4078
liuxingjun <connliu@qq.com>
2023-04-20 11:11:39 +0800

    add check File exists

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #! /bin/sh
 
-PWD=$(dirname $(readlink -f "$0"))
+PWD=$(dirname "$(readlink -f "$0")")
 
-for file in $PWD/dot_*; do
-    ln -sfv $file ~/.${file#*dot_}
+for dot_file in "$PWD"/dot_*; do
+    home_file=~/.${dot_file#*dot_}
+    if [ -e "$home_file" ]; then
+        real_file=$(readlink -f "$home_file")
+        if [ "$real_file" = "$home_file" ]; then
+            mv "$home_file" "$home_file.bak"
+        elif [ "$real_file" = "$dot_file" ]; then
+            rm "$home_file"
+        fi
+    fi
+    ln -sv $dot_file $home_file
 done
