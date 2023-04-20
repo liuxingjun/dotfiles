dcc0971f0afe5f6cc5322b8f71e3eba1c5ac0868
liuxingjun <connliu@qq.com>
2023-04-20 14:16:32 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -3,14 +3,18 @@
 PWD=$(dirname "$(readlink -f "$0")")
 
 for dot_file in "$PWD"/dot_*; do
-    home_file=~/.${dot_file#*dot_}
-    if [ -e "$home_file" ]; then
-        real_file=$(readlink -f "$home_file")
-        if [ "$real_file" = "$home_file" ]; then
-            mv "$home_file" "$home_file.bak"
-        elif [ "$real_file" = "$dot_file" ]; then
-            rm "$home_file"
-        fi
+    home_file="$HOME/.${dot_file#*dot_}"
+    if [ ! -e "$home_file" ]; then # 文件不存在
+        ln -sv "$dot_file" "$home_file"
+        continue
     fi
-    ln -sv $dot_file $home_file
+    real_file=$(readlink -f "$home_file")
+    if [ "$real_file" = "$dot_file" ]; then # 文件已经软链到目标文件
+        echo "skip file exists $home_file" 
+        continue
+    fi
+    if [ "$real_file" = "$home_file" ]; then # 文件非软链 或者 不是软链到目标文件
+        mv "$home_file" "$home_file.bak"
+        ln -sv "$dot_file" "$home_file"
+    fi
 done
