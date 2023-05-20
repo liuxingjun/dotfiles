5a036f9150331068af4c41467e300c3dbf0c6c64
liuxingjun <connliu@qq.com>
2023-05-20 09:52:42 +0800

    gitconfig 使用 include.path

--- install.sh
+++ install.sh
@@ -3,7 +3,12 @@
 PWD=$(dirname "$(readlink -f "$0")")
 
 for dot_file in "$PWD"/dot_*; do
-    home_file="$HOME/.${dot_file#*dot_}"
+    file_name=".${dot_file#*dot_}"
+    if [ "$file_name" = ".gitconfig" ]; then
+        git config --global include.path "$dot_file"
+        continue
+    fi
+    home_file="$HOME/$file_name"
     if [ ! -e "$home_file" ]; then # 文件不存在
         ln -sv "$dot_file" "$home_file"
         continue
