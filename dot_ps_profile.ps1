d8d43191bd6a4b17a02c3425a9a46c3325ee836f
liuxingjun <connliu@qq.com>
2024-07-15 14:21:33 +0800

    Create dot_ps_profile.ps1

---
+++ dot_ps_profile.ps1
@@ -0,0 +1,11 @@
+# New-Item -Path $profile -Value ./dot_ps_profile.ps1 -ItemType SymbolicLink -Force
+$env:DISPLAY = "127.0.0.1:0.0"
+function ssh-copy-id([string]$userAtMachine, $args){   
+    $publicKey = "$ENV:USERPROFILE" + "/.ssh/id_rsa.pub"
+    if (!(Test-Path "$publicKey")){
+        Write-Error "ERROR: failed to open ID file '$publicKey': No such file"            
+    }
+    else {
+        & cat "$publicKey" | ssh $args $userAtMachine "umask 077; test -d .ssh || mkdir .ssh ; cat >> .ssh/authorized_keys || exit 1"      
+    }
+}
